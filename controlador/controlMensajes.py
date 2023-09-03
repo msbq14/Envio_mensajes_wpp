fe40a1bc20a2a4fa541dd8be489727390dc2d54d
Belen Sarmiento <mbelen.sarmiento@ucuenca.edu.ec>
2023-09-03 09:38:11 -0500

    quitar codigo innecesario

--- controlador/controlMensajes.py
+++ controlador/controlMensajes.py
@@ -151,16 +151,10 @@ class Controlador(QWidget):
         #mostrar la vista previa en otra ventana puede ser mejor idea 
         mensaje = "¿Está seguro de que desea el mensaje a los numeros telefonicos que aparecen en la tabla?"
         respuesta = QMessageBox.question(self, "Confirmacion", mensaje, QMessageBox.StandardButton.Yes, QMessageBox.StandardButton.No)
-        print(respuesta)
-        if respuesta==16384:
-            print("no hay problema con la confirmacion")
-        if self.vista.ui.txtAreaMensaje.toPlainText().strip():
-            print("No hay problema con comprobar mensaje vacio")
-        if( self.verificarTablaNoVacia==True):
-            print("No hay problema con comprobar tabla vacia")
-
+        
 
         if self.vista.ui.txtAreaMensaje.toPlainText().strip() and respuesta == 16384:
+
             self.modelo.mandarMensaje(self.obtenerSegundaColumna(),self.vista.ui.txtAreaMensaje.toPlainText())
 
         else:
